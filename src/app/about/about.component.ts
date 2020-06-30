738325f024adb4477bbe9851a83c03389e9c969f
Jinhuan Lei <jinhuanlei@gmail.com>
2020-06-29 22:04:22 -0500

    init setup

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,46 @@
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {
+  concat,
+  fromEvent,
+  interval,
+  noop,
+  observable,
+  Observable,
+  of,
+  timer,
+  merge,
+  Subject,
+  BehaviorSubject,
+  AsyncSubject,
+  ReplaySubject
+} from 'rxjs';
+import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
+import {createHttpObservable} from '../common/util';
+
+
+@Component({
+  selector: 'about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  ngOnInit() {
+    document.addEventListener('click', event => {
+      console.log(event);
+    })
+
+    let counter = 0;
+    setInterval(() => {
+      console.log(counter);
+      counter++;
+    }, 1000)
+  }
+
+}
+
+
+
+
+
+
